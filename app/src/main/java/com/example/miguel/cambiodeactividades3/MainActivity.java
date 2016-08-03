db389618616c473e95c620926a462e3f915ef719
miguelmejia7 <migulin14@hotmail.com>
2016-08-03 16:16:02 -0600

    practica

---
+++ app/src/main/java/com/example/miguel/cambiodeactividades3/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.miguel.cambiodeactividades3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends  Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button boton = (Button) findViewById(R.id.button);
+
+
+        boton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        Intent a = new Intent(this,Main2Activity.class);
+        startActivity(a);
+    }
+}
